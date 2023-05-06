7ec30caa6dfdc7d22a3d9637a3308427b9c0bce5
Bulat Sharafutdinov <hufdgdgdgfd@mail.ru>
2023-05-06 18:51:37 +0300

    feature: добавить новый компонент - список комиксов

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -18,17 +19,20 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
+                {/*<ErrorBoundary>*/}
+                {/*    <RandomChar/>*/}
+                {/*</ErrorBoundary>*/}
+                {/*<div className="char__content">*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharList onCharSelected={onCharSelected} />*/}
+                {/*    </ErrorBoundary>*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharInfo charId={selectedChar} />*/}
+                {/*    </ErrorBoundary>*/}
+                {/*</div>*/}
                 <ErrorBoundary>
-                    <RandomChar/>
+                    <ComicsList />
                 </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundary>
-                </div>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -17,6 +17,18 @@ const useMarvelService = () => {
 		return _transformCharacter(res.data.results[0]);
 	}
 
+	const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformComics);
+	};
+
+	const getComics = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+		return _transformComics(res.data.results[0]);
+	};
+
 	const _transformCharacter = (char) => {
 		return {
 			id: char.id,
@@ -29,7 +41,24 @@ const useMarvelService = () => {
 		}
 	}
 
-	return { loading, error, clearError, getAllCharacters, getCharacter }
+	const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			// optional chaining operator
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
+
+	return { loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComics }
 }
 
 export default useMarvelService;
